0dfe9be821cb41056c449c2f3a4649b9dd343252
leiwen <657106593@qq.com>
2018-12-15 22:46:23 +0800

    修改客户端与服务端多次连接

--- www/socket_client.py
+++ www/socket_client.py
@@ -7,8 +7,21 @@ client=socket.socket()
 ip_port=("127.0.0.1",8888)
 #连接主机
 client.connect(ip_port)
-#接收主机信息
-data=client.recv(1024)
-#打印接收的数据
-#此处的byte型数据特指python3.x以上
-print(data.decode())
+
+while True:
+	#接收主机信息
+	data=client.recv(1024)
+	#打印接收的数据
+	#此处的byte型数据特指python3.x以上
+	print(data.decode())
+	#定义一个循环，不断的发送消息
+	#输入发送的消息
+	msg_input=input("请输入发送的消息：")
+	#消息发送
+	client.send(msg_input.encode())
+	if msg_input=="exit":
+		break
+	data=client.recv(1024)
+	print(data.decode())
+
+	
--- www/socket_server.py
+++ www/socket_server.py
@@ -1,5 +1,6 @@
 #导入模块
 import socket
+import random
 
 #创建实例
 sk=socket.socket()
@@ -9,15 +10,30 @@ ip_port=("127.0.0.1",8888)
 sk.bind(ip_port)
 #最大连接数
 sk.listen(5)
-#提升信息
-print("正在进行等待接受数据......")
-#接受数据
-conn, address=sk.accept()
-#定义信息
-msg="Hello World!"
-#返回信息
-#python3.x以上，网络数据的发送和接受都是byte类型
-#如果发送的数据是str型则需要进行编码
-conn.send(msg.encode())
-#主动关闭连接
-conn.close()
+#不断循环，不断接受数据
+while True:
+	#提升信息
+	print("正在进行等待接受数据......")
+	#接受数据
+	conn, address=sk.accept()
+	#定义信息
+	msg="连接成功!"
+	#返回信息
+	#python3.x以上，网络数据的发送和接受都是byte类型
+	#如果发送的数据是str型则需要进行编码
+	conn.send(msg.encode())
+	#不断接收客户端发来的消息
+	while True:
+		#接收客户端消息
+		data=conn.recv(1024)
+		#打印数据
+		print(data.decode())
+		#接收到退出指令
+		if data==b'exit':
+			break
+		#处理客户端数据
+		conn.send(data)
+		#发送随机数信息
+		conn.send(str(random.randint(1,1000)).encode())
+	#主动关闭连接
+	conn.close()
